79446d3f9ae03f88431efb7929e0d781ef4078c5
Maurina Davis <Maurina.Davis@gmail.com>
2019-02-06 10:47:01 -0700

    images added click function fixed

--- main.js
+++ main.js
@@ -1,78 +1,79 @@
-let response = document.getElementById('myGuess').value;
-console.log(response);
-let guess = Number(response);
-console.log (guess);
+// random number between 1 and 15
 let answer = Math.floor((Math.random()*15)+1);
 console.log(answer);
-numberOfGuess = 0;
 
+
+let guess;
+
+
+  
 
 function guessingGame(){
-   //let resposeGuess = document.querySelector('results')
+ //input guess from user------------------------------------------
+    guess = document.getElementById('myGuess').value;
+    let numberOfGuess = 0;
+    console.log (guess);
+// check if input is between 1-15---------------------------------
     if (guess < 1 || guess > 15){
         document.getElementById('results').innerHTML = "You must enter a number between 1 and 15";
-        //let outOfRange = document.createElement('p')
-        //outOfRange.textContent = "You must enter a number between 1 and 15"
-        //resposeGuess.appendChild(outOfRange)
+     console.log("You must enter a number between 1 and 15");
      
-      console.log("You must enter a number between 1 and 15");
-      return;
-    }
-    else if (guess === answer){
-        //let correct = document.createElement('p')
-        //correct.textContent = "You are correct"
-        //resposeGuess.appendChild(correct)
+    }//end check
+
+
+//correct answer-------------------------------------------------    
+        else if (guess == answer){
         document.getElementById('results').innerHTML = "You are correct";
+//add to number of guesses        
+       numberOfGuess++;
+        document.getElementById('tries').innerHTML = `Tries: ${numberOfGuess}`;
+        console.log(numberOfGuess);
             console.log("You are correct")
+//awards depending on the number of guesses-----------------------         
             switch(numberOfGuess){
                 case 1:
                 case 2:
                 case 3:
-                let awardImage = docuement.createElement("img");
-                let imageSource = "images/blue.jpg";
-                docuement.getElementById('Award').appendChild(awardImage);
+                let awardImage = document.createElement("img")
+                awardImage.src ="images/red.png";
+                 document.getElementById('Award').appendChild(awardImage);
+                 break;
                 case 4:
                 case 5:
                 case 6:
-                let awardImageRed = docuement.createElement("img");
-                let imageSources = "images/red.jpg";
-                docuement.getElementById('Award').appendChild(awardImage);
+                let awardImageBlue = document.createElement("img");
+                awardImageBlue.src = "images/blue.png"
+                document.getElementById('Award').appendChild(awardImageBlue);
+                break;
                 default:
-                let awardImageGold = docuement.createElement("img");
-                let imgSource = "images/gold.jpg";
-                docuement.getElementById('Award').appendChild(awardImage);
+                let awardImageGold = document.createElement("img");
+                awardImageGold.scr  = "images/gold.png";
+                document.getElementById('Award').appendChild(awardImageGold);
             }//end switch
-            return;
+        
 
            }
-    else if (guess > answer){
-      //  let answerHigh = document.createElement('p')
-       // answerHigh.textContent = "Your guess is too high"
-       // resposeGuess.appendChild(answerHigh)
-       document.getElementById('results').innerHTML = "You are too high";
-
+//guess is too high
+        else if (guess > answer){
+        document.getElementById('results').innerHTML = "You are too high";
+//add to number of guesses  
+        numberOfGuess++;
+        document.getElementById('tries').innerHTML =  `Tries: ${numberOfGuess}`;
+        console.log(numberOfGuess);
         console.log("You are high")
      
     }
-    else {
-        //let answerLow = document.createElement('p')
-        //answerLow.textContent = "Your guess is too low"
-        //resposeGuess.appendChild(answerLow)
+// guess is too low    
+        else {
         document.getElementById('results').innerHTML = "You are too low";
+//add to number of guesses  
+        numberOfGuess++;
+        document.getElementById('tries').innerHTML =  `Tries: ${numberOfGuess}`;
+        console.log(numberOfGuess);
         console.log("You are low")
       
-    }
-    
-    numbers();
-    console.log(numbers)
-    document.getElementById('tries').innerHTML = numbers;
+        }
+
 }//end function
-
-function numbers () {
-
-    while (guess != answer){
-        numberOfGuess = numberOfGuess+1;
-    }
-}//end numbers
 
 
